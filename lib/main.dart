0415a79662f8ee3b94ea0ede3d521ee58ac2d24b
lexara-prime-ai <irfanghat@gmail.com>
2024-08-29 13:37:36 +0300

    Update data structure examples.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
+void printListContent(List list) {
+  for (var i = 0; i < list.length; i++) {
+    print(list[i]);
+  }
+}
+
 void main() {
   // Defining variables.
   String name = "John Doe";
@@ -8,8 +14,10 @@ void main() {
   double pi = 3.14;
   bool isTemplate = true;
 
+  List nums = [1, 2, 3, 4, 5];
+
   if (kDebugMode) {
-    print(name);
+    printListContent(nums);
   }
 
   /*
@@ -33,6 +41,10 @@ void main() {
 
     The NOT operator returns the opposite value.
     !isTemplate -> false;
+
+    List -> Array
+    Set -> An array of unique values.
+    Map -> Key value pairs e.g HashMap, Dict.
   */
 
   runApp(const App());
