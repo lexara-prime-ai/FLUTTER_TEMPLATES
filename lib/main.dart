a0050c3fe6e77ff2203403268a6468c238fac856
lexara-prime-ai <irfanghat@gmail.com>
2024-08-29 11:56:36 +0300

    Added const keyword to constructor invocation to improve perfoemance

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
