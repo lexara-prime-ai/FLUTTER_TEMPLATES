71ff77cee800c70eb1919e7980fb37758587310b
lexara-prime-ai <irfanghat@gmail.com>
2024-08-29 21:49:06 +0300

    Gesture detector.

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,8 @@ List<String> getUsers() {
   return ["Johnny", "Potter", "Annie", "Walker", "Woistreshaur"];
 }
 
+int count = 0;
+
 class App extends StatelessWidget {
   const App({super.key});
 
@@ -30,13 +32,28 @@ class App extends StatelessWidget {
                 ))
           ],
         ),
-        body: GridView.builder(
-          itemCount: 64,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4),
-          itemBuilder: (context, index) => Container(
-            color: Colors.amber[200],
-            margin: EdgeInsets.all(2),
+        body: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(21),
+              color: Colors.amber[300],
+            ),
+            height: 200,
+            width: 200,
+            child: GestureDetector(
+              onTap: () {
+                print(count++);
+              },
+              child: const Center(
+                child: Text(
+                  "Tap me!",
+                  style: TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+              ),
+            ),
           ),
         ),
       ),
