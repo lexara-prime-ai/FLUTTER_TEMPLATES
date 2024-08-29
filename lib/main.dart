fd72a99c1ad52d5baca15d35177b70eddad96a40
lexara-prime-ai <irfanghat@gmail.com>
2024-08-29 14:39:31 +0300

    Widgets: appBar, body, Container, Text, Icon, IconButton.

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,38 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        backgroundColor: Colors.amber[100],
+        appBar: AppBar(
+          title: const Text("Music"),
+          backgroundColor: Colors.amber[200],
+          elevation: 0,
+          leading: const Icon(Icons.menu_open),
+          actions: const [
+            IconButton(
+                onPressed: null,
+                icon: Icon(
+                  Icons.logout,
+                  color: Colors.black87,
+                ))
+          ],
+        ),
+        body: Center(
+          child: Container(
+              height: 300,
+              width: 300,
+              decoration: BoxDecoration(
+                  color: Colors.amber[300],
+                  borderRadius: BorderRadius.circular(32)),
+              padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 24),
+              child: const Icon(
+                Icons.headphones,
+                size: 72,
+              )),
+        ),
+      ),
     );
   }
 }
