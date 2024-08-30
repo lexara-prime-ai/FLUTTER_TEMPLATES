3122857500f323c0f6333daabd4b2843eb480808
lexara-prime-ai <irfanghat@gmail.com>
2024-08-30 09:40:59 +0300

    Bottom navigation implementation.

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:test_drive/pages/home_page.dart';
+import 'package:test_drive/pages/profile_page.dart';
+import 'package:test_drive/pages/settings_page.dart';
 
-class FirstPage extends StatelessWidget {
-  const FirstPage({super.key});
+class FirstPage extends StatefulWidget {
+  FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  int _selectedIndex = 0;
+
+  final List _pages = [HomePage(), ProfilePage(), SettingsPage()];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("1st Page"),
+        backgroundColor: Colors.amber[200],
       ),
+      body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavigationBar(
-        items: [
+        currentIndex: _selectedIndex,
+        onTap: navigateBottomBar,
+        items: const [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(
               icon: Icon(Icons.person_2_outlined), label: "Profile"),
