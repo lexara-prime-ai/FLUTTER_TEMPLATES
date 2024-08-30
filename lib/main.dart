0498e5bb12183476b34fbee98aa222aa723465d4
lexara-prime-ai <irfanghat@gmail.com>
2024-08-30 11:12:16 +0300

    Working with Text Fields.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:test_drive/pages/todo_page.dart';
 
 void main() {}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: TodoPage(),
+    );
+  }
+}
