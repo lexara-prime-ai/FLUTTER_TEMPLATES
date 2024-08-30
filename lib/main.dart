e365a0b8072e21038f052c76d3a681553cf005aa
lexara-prime-ai <irfanghat@gmail.com>
2024-08-30 09:45:08 +0300

    Blank template.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,3 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(),
-    );
-  }
-}
+void main() {}
